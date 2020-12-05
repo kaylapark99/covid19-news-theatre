aa6b4d6fca856a100cf0383929c0db66b1234629
kaylapark99 <kpark@colgate.edu>
2020-12-05 14:53:31 -0500

    designing index

--- Tech/scripts.js
+++ Tech/scripts.js
@@ -6,15 +6,23 @@ $(document).ready(function(){
         beforeSend: function(xhr){xhr.setRequestHeader('x-access-token', '5cf9dfd5-3449-485e-b5ae-70a60e997864');
         },
     }).done(function(data){
+
+            function formatDate(){
+                var dateArray = data['Date'].substring(0,10).split('-');
+                var date = dateArray[1] + '/' + dateArray[2] + '/' + dateArray[0];
+                var time = data['Date'].substring(11,19);
+                return date + ' ' + time;
+            }
+            
             //updates global stats, toggle between today's stats vs overall
-            $("#last-update").html(data['Date']);
+            $("#last-update").html(formatDate());
             $("#total-cases").html(data['Global']['TotalConfirmed'].toLocaleString());
             $("#total-deaths").html(data['Global']['TotalDeaths'].toLocaleString());
             $("#total-recovered").html(data['Global']['TotalRecovered'].toLocaleString());
             $("#today-cases").html(data['Global']['NewConfirmed'].toLocaleString());
             $("#today-deaths").html(data['Global']['NewDeaths'].toLocaleString());
             $("#today-recovered").html(data['Global']['NewRecovered'].toLocaleString());
-    
+      
             $.fn.toggleGlobal = function(x,y,z) {
                 $(z).addClass('pressed');
                 $(this).removeClass('pressed');
@@ -30,6 +38,15 @@ $(document).ready(function(){
                 countries.push(data['Countries'][c]['Country']);
             }
 
+            //find the index of the country within country arrays
+            function findIndex(searched) {
+                var indexCountry = countries.indexOf(searched);
+                if(indexCountry == -1){
+                    indexCountry = slugCountries.indexOf(searched);
+                }
+                return indexCountry;
+            }
+
             function findCountries(search) {
                 return countries.filter(c => {
                     const regex = RegExp(search, 'i');
@@ -37,46 +54,65 @@ $(document).ready(function(){
                 });
             };
 
+            //display dropdown menu
             function displayCountries() {
                 var searchCountries = findCountries(this.value);
                 html = searchCountries.map(c => {
-                    if(this.value == '') {
+                    if(this.value == '' || findIndex(this.value) != -1) {
                         return ``
                     }
                     return `
-                        <li><a href="#">
-                            <span class="search-country" onclick="alert('hi')">${c}</span>
-                        </a></li>  
+                        <li>
+                            <span class="search-country">${c}</span>
+                        </li>  
                     `;
                 }).join('');
                 
                 $('#countries-list').html(html); 
             }
 
+            //events to trigger dropdown search
             var inputCountry = $('.search');
             inputCountry.on('keyup',displayCountries);
             inputCountry.on('paste',displayCountries);
-2
+
             //update results based off of selected country 
+            function updateCountryData(cIndex) {
+                var countryData = data['Countries'][cIndex];
+                $("#total-country-cases").html(countryData['TotalConfirmed'].toLocaleString());
+                $("#total-country-deaths").html(countryData['TotalDeaths'].toLocaleString());
+                $("#total-country-recovered").html(countryData['TotalRecovered'].toLocaleString());
+                $("#today-country-cases").html(countryData['NewConfirmed'].toLocaleString());
+                $("#today-country-deaths").html(countryData['NewDeaths'].toLocaleString());
+                $("#today-country-recovered").html(countryData['NewRecovered'].toLocaleString());
+                $('#countries-list').html(''); 
+                $('.search').val(data['Countries'][cIndex]['Country']);
+            }
+
+            //check to see if input is a country
+            function checkInput(input) {
+                var indexCountry = findIndex(input)
+                if(indexCountry != -1){
+                    updateCountryData(indexCountry);
+                }
+                else{
+                    alert(input + ' is not in the database!');
+                }
+            }
+
             inputCountry.on('keypress',function(e){
                 if(e.which == 13) {
                     var searchFor = inputCountry.val();
-                    var indexCountry = countries.indexOf(searchFor);
-                    if(indexCountry == -1){
-                        indexCountry = slugCountries.indexOf(searchFor);
-                        if(indexCountry == -1){
-                            alert("Country name not in database!");
-                        }
-                    }
-                    var countryData = data['Countries'][indexCountry];
-                    $("#total-country-cases").html(countryData['TotalConfirmed'].toLocaleString());
-                    $("#total-country-deaths").html(countryData['TotalDeaths'].toLocaleString());
-                    $("#total-country-recovered").html(countryData['TotalRecovered'].toLocaleString());
-                    $("#today-country-cases").html(countryData['NewConfirmed'].toLocaleString());
-                    $("#today-country-deaths").html(countryData['NewDeaths'].toLocaleString());
-                    $("#today-country-recovered").html(countryData['NewRecovered'].toLocaleString());
+                    checkInput(searchFor);
                 }
             });
+
+            //enable clicking on dropdown to select country
+            $('#countries-list').on('click','li',(function(){
+                var countryName = $(this).closest('li').find('span').text();
+                checkInput(countryName);
+
+            }));
 
         });
 });
