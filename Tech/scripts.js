02988732893eb53183662ca82735d0a66cc05a3d
kaylapark99 <kpark@colgate.edu>
2020-12-13 18:09:23 -0500

    adding twitter list

--- Tech/scripts.js
+++ Tech/scripts.js
@@ -18,124 +18,129 @@ $(document).ready(function(){
         async: false,
         beforeSend: function(xhr){xhr.setRequestHeader('x-access-token', '5cf9dfd5-3449-485e-b5ae-70a60e997864');
         },
-    }).done(function(data){
-
-        function formatDate(){
-            var dateArray = data['Date'].substring(0,10).split('-');
-            var date = dateArray[1] + '/' + dateArray[2] + '/' + dateArray[0];
-            var time = data['Date'].substring(11,19);
-            return date + ' ' + time;
-        }
-
-        todayDeaths = data['Global']['NewDeaths'];
-        
-        //updates global stats, toggle between today's stats vs overall
-        $("#last-update").html(formatDate());
-        $("#total-cases").html(data['Global']['TotalConfirmed'].toLocaleString());
-        $("#total-deaths").html(data['Global']['TotalDeaths'].toLocaleString());
-        $("#total-recovered").html(data['Global']['TotalRecovered'].toLocaleString());
-        $("#today-cases").html(data['Global']['NewConfirmed'].toLocaleString());
-        $("#today-deaths").html(todayDeaths.toLocaleString());
-        $("#today-recovered").html(data['Global']['NewRecovered'].toLocaleString());
+        success: function(data) {
+            function formatDate(){
+                var dateArray = data['Date'].substring(0,10).split('-');
+                var date = dateArray[1] + '/' + dateArray[2] + '/' + dateArray[0];
+                var time = data['Date'].substring(11,19);
+                return date + ' ' + time;
+            }
     
-        $.fn.toggleGlobal = function(x,y,z) {
-            $(z).addClass('pressed');
-            $(this).removeClass('pressed');
-            $(x).removeClass('hide');
-            $(y).addClass('hide');
-        };
-
-        //search for country
-        var slugCountries = []
-        var countries = []
-        for(c in data['Countries']){
-            slugCountries.push(data['Countries'][c]['Country'].toLowerCase());
-            countries.push(data['Countries'][c]['Country']);
-        }
-
-        //find the index of the country within country arrays
-        function findIndex(searched) {
-            var indexCountry = countries.indexOf(searched);
-            if(indexCountry == -1){
-                indexCountry = slugCountries.indexOf(searched);
-            }
-            return indexCountry;
-        }
-
-        function findCountries(search) {
-            return countries.filter(c => {
-                const regex = RegExp(search, 'i');
-                return c.match(regex);
-            });
-        };
-
-        //display dropdown menu
-        function displayCountries() {
-            var searchCountries = findCountries(this.value);
-            var html = searchCountries.map(c => {
-                if(this.value == '' || findIndex(this.value) != -1) {
-                    return ``
-                }
-                return `
-                    <li>
-                        <span class="search-country">${c}</span>
-                    </li>  
-                `;
-            }).join('');
+            todayDeaths = data['Global']['NewDeaths'];
             
-            $('#countries-list').html(html); 
-        }
-
-        //events to trigger dropdown search
-        var inputCountry = $('.search');
-        inputCountry.on('keyup',displayCountries);
-        inputCountry.on('paste',displayCountries);
-
-        function updateGraph(cIndex){
-            var htmlFirst = '<iframe src="https://ourworldindata.org/grapher/total-deaths-covid-19?country='
-            var htmlLast = '" style="width: 100%; height: 600px; border: 3px solid;"></iframe>'
-            var htmlAlpha = countryCodes[countries[cIndex]]
-            $('.graph').html(htmlFirst+htmlAlpha+htmlLast);
-        }
-
-        //update results based off of selected country 
-        function updateCountryData(cIndex) {
-            var countryData = data['Countries'][cIndex];
-            $("#total-country-cases").html(countryData['TotalConfirmed'].toLocaleString());
-            $("#total-country-deaths").html(countryData['TotalDeaths'].toLocaleString());
-            $("#total-country-recovered").html(countryData['TotalRecovered'].toLocaleString());
-            $("#today-country-cases").html(countryData['NewConfirmed'].toLocaleString());
-            $("#today-country-deaths").html(countryData['NewDeaths'].toLocaleString());
-            $("#today-country-recovered").html(countryData['NewRecovered'].toLocaleString());
-            $('#countries-list').html(''); 
-            $('.search').val(data['Countries'][cIndex]['Country']);
-            updateGraph(cIndex);
-        }
-
-        //check to see if input is a country
-        function checkInput(input) {
-            var indexCountry = findIndex(input)
-            if(indexCountry != -1){
-                updateCountryData(indexCountry);
+            //updates global stats, toggle between today's stats vs overall
+            $("#last-update").html(formatDate());
+            $("#total-cases").html(data['Global']['TotalConfirmed'].toLocaleString());
+            $("#total-deaths").html(data['Global']['TotalDeaths'].toLocaleString());
+            $("#total-recovered").html(data['Global']['TotalRecovered'].toLocaleString());
+            $("#today-cases").html(data['Global']['NewConfirmed'].toLocaleString());
+            $("#today-deaths").html(todayDeaths.toLocaleString());
+            $("#today-recovered").html(data['Global']['NewRecovered'].toLocaleString());
+        
+            $.fn.toggleGlobal = function(x,y,z) {
+                $(z).addClass('pressed');
+                $(this).removeClass('pressed');
+                $(x).removeClass('hide');
+                $(y).addClass('hide');
+            };
+    
+            //search for country
+            var slugCountries = []
+            var countries = []
+            for(c in data['Countries']){
+                slugCountries.push(data['Countries'][c]['Country'].toLowerCase());
+                countries.push(data['Countries'][c]['Country']);
             }
-            else{
-                alert(input + ' is not in the database!');
+    
+            //find the index of the country within country arrays
+            function findIndex(searched) {
+                var indexCountry = countries.indexOf(searched);
+                if(indexCountry == -1){
+                    indexCountry = slugCountries.indexOf(searched);
+                }
+                return indexCountry;
             }
+    
+            function findCountries(search) {
+                return countries.filter(c => {
+                    const regex = RegExp(search, 'i');
+                    return c.match(regex);
+                });
+            };
+    
+            //display dropdown menu
+            function displayCountries() {
+                var searchCountries = findCountries(this.value);
+                var html = searchCountries.map(c => {
+                    if(this.value == '' || findIndex(this.value) != -1) {
+                        return ``
+                    }
+                    return `
+                        <li>
+                            <span class="search-country">${c}</span>
+                        </li>  
+                    `;
+                }).join('');
+                
+                $('#countries-list').html(html); 
+            }
+    
+            //events to trigger dropdown search
+            var inputCountry = $('.search');
+            inputCountry.on('keyup',displayCountries);
+            inputCountry.on('paste',displayCountries);
+    
+            function updateGraph(cIndex){
+                var htmlFirst = '<iframe src="https://ourworldindata.org/grapher/total-deaths-covid-19?country='
+                var htmlLast = '" style="width: 100%; height: 600px; border: 3px solid;"></iframe>'
+                var htmlAlpha = countryCodes[countries[cIndex]]
+                $('.graph').html(htmlFirst+htmlAlpha+htmlLast);
+            }
+    
+            //update results based off of selected country 
+            function updateCountryData(cIndex) {
+                var countryData = data['Countries'][cIndex];
+                $("#total-country-cases").html(countryData['TotalConfirmed'].toLocaleString());
+                $("#total-country-deaths").html(countryData['TotalDeaths'].toLocaleString());
+                $("#total-country-recovered").html(countryData['TotalRecovered'].toLocaleString());
+                $("#today-country-cases").html(countryData['NewConfirmed'].toLocaleString());
+                $("#today-country-deaths").html(countryData['NewDeaths'].toLocaleString());
+                $("#today-country-recovered").html(countryData['NewRecovered'].toLocaleString());
+                $('#countries-list').html(''); 
+                $('.search').val(data['Countries'][cIndex]['Country']);
+                updateGraph(cIndex);
+            }
+    
+            //check to see if input is a country
+            function checkInput(input) {
+                var indexCountry = findIndex(input)
+                if(indexCountry != -1){
+                    updateCountryData(indexCountry);
+                }
+                else{
+                    alert(input + ' is not in the database!');
+                }
+            }
+    
+            inputCountry.on('keypress',function(e){
+                if(e.which == 13) {
+                    var searchFor = inputCountry.val();
+                    checkInput(searchFor);
+                }
+            });
+    
+            //enable clicking on dropdown to select country
+            $('#countries-list').on('click','li',(function(){
+                var countryName = $(this).closest('li').find('span').text();
+                checkInput(countryName);
+    
+            }));
+        },
+        error: function(){
+            alert("Covid API Network Error!")
         }
 
-        inputCountry.on('keypress',function(e){
-            if(e.which == 13) {
-                var searchFor = inputCountry.val();
-                checkInput(searchFor);
-            }
-        });
-
-        //enable clicking on dropdown to select country
-        $('#countries-list').on('click','li',(function(){
-            var countryName = $(this).closest('li').find('span').text();
-            checkInput(countryName);
-
-        }));
+        
 
     });
 
@@ -194,32 +199,34 @@ $(document).ready(function(){
                 var links = $('.article-link')
 
                 var articleNum = 0;
-                for(l=0; l<=5; l++) {
-                    //api has repeat articles
-                    while(data['news'][articleNum]['title'] === data['news'][articleNum+1]['title']){
-                        articleNum++;
-                    }
-                    var articleURL = data['news'][articleNum]['url'];
-                    var title = data['news'][articleNum]['title'];
-                    var link = '<a href="' + articleURL + '" target="_blank">'+ title + '</a>';
-                    var author = data['news'][articleNum]['author'];
-                    var image = data['news'][articleNum]['image'];
-                    var description = data['news'][articleNum]['description'];
-                    var date = data['news'][articleNum]['published'].substring(0,19);
-                    
-                    if(image!=='None') {
-                        var imgSrc = '<img src="'+image + '"/>';
-                        $(images[l]).html(imgSrc);
-                    }
+                var articleCount = 0;
+                var articleTitles = [];
                 
-                    $(links[l]).html(link);
-                    author = author.replace('-',' ');
-                    $(authors[l]).html('By: ' + author);
-                    $(descriptions[l]).html(description.toLocaleString());
-                    $(dates[l]).html('Published: ' + date.toLocaleString());
+                while(articleCount != 8) {
+                    console.log(data['news'][articleNum]['title']);
+                    var title = data['news'][articleNum]['title'];
+                    if(!articleTitles.includes(title)){
+                        articleTitles.push(title);
+                        var articleURL = data['news'][articleNum]['url'];
+                        var link = '<a href="' + articleURL + '" target="_blank">'+ title + '</a>';
+                        var author = data['news'][articleNum]['author'];
+                        var image = data['news'][articleNum]['image'];
+                        var description = data['news'][articleNum]['description'];
+                        var date = data['news'][articleNum]['published'].substring(0,19);
+
+                        if(image!=='None') {
+                            var imgSrc = '<img src="'+image + '"/>';
+                            $(images[articleCount]).html(imgSrc);
+                        }
+                        $(links[articleCount]).html(link);
+                        author = author.replace('-',' ');
+                        $(authors[articleCount]).html('By: ' + author);
+                        $(descriptions[articleCount]).html(description.toLocaleString());
+                        $(dates[articleCount]).html('Published: ' + date.toLocaleString());
+                        articleCount++;
+                    }
                     articleNum++;
                 }
-
 
             },
             error: function() {
@@ -228,6 +235,7 @@ $(document).ready(function(){
                 }
             }
         });
+        
     }
     
 });
